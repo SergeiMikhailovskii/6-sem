1c9bf57783a034cc1cdc815242003769513eab77
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-01-31 21:18:57 +0300

    Finished 1st task

--- киоки/Lab1/src/com/mikhailovskii/lab1/Main.java
+++ киоки/Lab1/src/com/mikhailovskii/lab1/Main.java
@@ -3,11 +3,11 @@ package com.mikhailovskii.lab1;
 public class Main {
 
     public static void main(String[] args) {
-        String m = "Cryptography";
-        String c = "";
+        String m = "Cryptography";  // Input string
+        String c = "";  // Encrypted string
 
         int length = m.length();
-        int k = 3;
+        int k = 4;
 
         String[][] arr = new String[k][length];
 
@@ -38,6 +38,58 @@ public class Main {
         }
 
         System.out.println(c);
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < length; j++) {
+                arr[i][j] = "";
+            }
+        }
+
+        x = 0;
+        isDown = false;
+
+        for (int i = 0; i < length; i++) {
+            arr[x][i] = "*";
+            if (x == k - 1 || x == 0) {
+                isDown = !isDown;
+            }
+            if (isDown) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+
+        int current = 0;
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < length; j++) {
+                if (arr[i][j].equals("*")) {
+                    arr[i][j] = String.valueOf(c.charAt(current));
+                    current++;
+                }
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print("[" + i + "]" + "[" + j + "]:" + arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        String out = "";
+        for (int i = 0; i < length; i++) {
+            out += arr[x][i];
+            if (x == k - 1 || x == 0) {
+                isDown = !isDown;
+            }
+            if (isDown) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        System.out.println(out);
 
     }
 }
