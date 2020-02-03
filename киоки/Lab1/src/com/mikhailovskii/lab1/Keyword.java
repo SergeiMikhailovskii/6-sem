ed6bf464bde1ff1ea1d2ef7f72aaf865f91443b3
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-02-03 18:44:56 +0300

    Second task part 1

--- киоки/Lab1/src/com/mikhailovskii/lab1/Keyword.java
+++ киоки/Lab1/src/com/mikhailovskii/lab1/Keyword.java
@@ -6,40 +6,70 @@ public class Keyword {
         String keyword = "CRYPTOGRAPHY";
         String m = "This is encryption algorithms lecture";
 
-        int length = keyword.length();
-        int mLength = m.length();
+        char[] keywordCharArray = keyword.toCharArray();
+        char[] mCharArray = m.toCharArray();
 
-        System.out.println(length + " " + mLength);
+        int rows = m.length() / keyword.length() + (m.length() % keyword.length() == 0 ? 0 : 1);
+        char[][] mMatrix = new char[rows][keyword.length()];
 
-        int addedSymbols = mLength % length;    // Calculate how many symbols should we add to fill the input string
-        if (addedSymbols != 0) {
-            addedSymbols = length - addedSymbols;
-        }
-
-        for (int i = 0; i < addedSymbols; i++) {
-            m += " ";
-        }
-
-        System.out.println(m.length());
-
-        int rows = m.length() / length;
-        String[][] arr = new String[rows][length];
-
-        int curSymbol = 0;
+        int curElement = 0;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < length; j++) {
-                arr[i][j] = String.valueOf(m.charAt(curSymbol));
-                curSymbol++;
+            for (int j = 0; j < keyword.length(); j++) {
+                if (curElement < m.length()) {
+                    mMatrix[i][j] = mCharArray[curElement];
+                    curElement++;
+                } else {
+                    mMatrix[i][j] = Character.MIN_VALUE;
+                }
             }
         }
 
+        showMatrix(keyword, keywordCharArray, rows, mMatrix);
+
+        for (int i = 0; i < keyword.length() - 1; i++) {
+            for (int j = i + 1; j < keyword.length(); j++) {
+                if (keywordCharArray[i] > keywordCharArray[j]) {
+                    char temp = keywordCharArray[i];
+                    keywordCharArray[i] = keywordCharArray[j];
+                    keywordCharArray[j] = temp;
+
+                    for (int k = 0; k < rows; k++) {
+                        temp = mMatrix[k][i];
+                        mMatrix[k][i] = mMatrix[k][j];
+                        mMatrix[k][j] = temp;
+                    }
+                }
+            }
+        }
+
+        showMatrix(keyword, keywordCharArray, rows, mMatrix);
+
+        String output = "";
+
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < length; j++) {
-                System.out.print(arr[i][j] + " ");
+            for (int j = 0; j < keyword.length(); j++) {
+                output += mMatrix[i][j];
+            }
+        }
+
+        System.out.println(output);
+
+    }
+
+    private static void showMatrix(String keyword, char[] keywordCharArray, int rows, char[][] mMatrix) {
+        for (int i = 0; i < keyword.length(); i++) {
+            System.out.print(keywordCharArray[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < keyword.length(); j++) {
+                System.out.print(mMatrix[i][j] + " ");
             }
             System.out.println();
         }
 
+        System.out.println();
     }
 
 }
